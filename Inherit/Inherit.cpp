ddd288665d2a8ec7c978c9848fced26b332269c3
k6ra <m98213@gmail.com>
2019-03-23 20:30:40 +0900

    Create

---
+++ Inherit/Inherit.cpp
@@ -0,0 +1,44 @@
+﻿#include <Windows.h>
+
+int main()
+{
+	STARTUPINFO si = {};
+	SECURITY_ATTRIBUTES saProcess, saThread;
+	PROCESS_INFORMATION piProcessB, piProcessC;
+
+	saProcess.nLength = sizeof(saProcess);
+	saProcess.lpSecurityDescriptor = NULL;
+	saProcess.bInheritHandle = TRUE;
+
+	saThread.nLength = sizeof(saThread);
+	saThread.lpSecurityDescriptor = NULL;
+	saThread.bInheritHandle = FALSE;
+
+	TCHAR szPathB[MAX_PATH];
+	ua_tcscpy_s(szPathB, MAX_PATH * sizeof(TCHAR), TEXT("ProcessB"));
+	CreateProcess(
+		NULL,
+		szPathB,
+		&saProcess,
+		&saThread,
+		FALSE,
+		0,
+		NULL,
+		NULL,
+		&si,
+		&piProcessB);
+
+	TCHAR szPathC[MAX_PATH];
+	ua_tcscpy_s(szPathC, MAX_PATH * sizeof(TCHAR), TEXT("ProcessC"));
+	CreateProcess(
+		NULL,
+		szPathC,
+		NULL,
+		NULL,
+		TRUE,
+		0,
+		NULL,
+		NULL,
+		&si,
+		&piProcessC);
+}
